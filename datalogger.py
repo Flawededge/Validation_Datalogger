ff175e2e24c83ff5eeb2932a4b56bf8750b28fb0
Chris Diggle <crdiggle@gmail.com>
2021-05-17 23:00:13 +1200

    Building out the xml parser and turning it into a table/column data structure

--- datalogger.py
+++ datalogger.py
@@ -1,14 +1,48 @@
 from mysql.connector import connect, Error
-import xml
+import xml.etree.ElementTree as ET
+from dataclasses import dataclass, field
+
+@dataclass
+class table:
+    """ Class for storing a table """
+    name: str
+    datetime: bool
+    id: bool
+
+    columns = list("")
+
+    def create_query(self):
+        pass
+
+    def add_column(self, name, datatype):
+        self.columns.append(column(name=name, datatype=datatype))
+
+    def make_query(self):
+        [i.make_sql_line() for i in self.columns]
+
+@dataclass
+class column:
+    """ Class for storing a column """
+    name: str
+    datatype: str 
+
+    def make_sql_line(self):
+        """ Generates the SQL line to create this column
+        """
+        return f"{self.name} {self.datatype}"
+
 
 class datalogger():
     connection = None
-    def __init__(self, host, username, password, database):
+    schema = []
+    def __init__(self, host, username, password, database, schema_file):
         self.host = host
         self.username = username
         self.password = password
         self.database = database
+        self.schema_file = schema_file
 
+        self.generate_schema()
         self.connect()
         # self.schema_status = self.check_schema
 
@@ -22,7 +56,8 @@ class datalogger():
             self.connection = connect(
                 host=self.host,
                 user=self.username,
-                password=self.password)
+                password=self.password,
+                database=self.database)
         except Error as e:
             print(e)
             self.connected = False
@@ -33,8 +68,9 @@ class datalogger():
         
 
     def disconnect(self):
-        self.connection.close()
-        self.connection = None
+        if self.connection is not None:
+            self.connection.close()
+            self.connection = None
         self.connected = False
 
     def query(self, query):
@@ -49,11 +85,44 @@ class datalogger():
         if not self.connected:  # Disconnect after to clean up the connection
             self.disconnect()
 
+    def generate_schema(self):
+        """ Generates a schema from the given .xml file
+            ! Needs to be run each startup
+        """
+        tree = ET.parse(self.schema_file)
+        root = tree.getroot()
+
+        schema = []  # Clear the schema
+        for cur_table in root:
+            if "datetime" in cur_table.attrib.keys() and cur_table.attrib['datetime'] == "True": 
+                datetime = True
+            else: 
+                datetime = False
+            if "id" in cur_table.attrib.keys() and cur_table.attrib['id'] == "True": 
+                id = True
+            else: 
+                id = False
+
+            self.schema.append(table(name=cur_table.attrib["name"], datetime=datetime, id=id))
+
+            for cur_column in cur_table:
+                self.schema[-1].add_column(cur_column.attrib['name'], cur_column.text)
+        
+
 
     def __run_query(self, query):
         """ Actually runs the query
         """
+        pass
         
+    def create_database(self):
+        pass
+    
+    def create_schema(self):
+        pass
+
+    def create_schema(self):
+        pass
 
     def check_schema(self):
         """ Checks that the schema is valid to the .xml file
@@ -69,5 +138,11 @@ if __name__ == "__main__":
         password = cred[2]
 
     database = "Test_data"
-    data = datalogger(host, username, password, database)
+    schema_file = "data_type.xml"
+    data = datalogger(
+        host=host, 
+        username=username, 
+        password=password, 
+        database=database, 
+        schema_file=schema_file)
     print(data.connection_status)
